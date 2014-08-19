b4a6d1ca5bf6b2657c6eb21cc460e07e32782411
rscottm <rscottmoyer@gmail.com>
2014-08-18 20:19:43 -0700

    Skip adding command_type (moved to Base)

--- lib/ev3/commands/sound_tone.rb
+++ lib/ev3/commands/sound_tone.rb
@@ -7,7 +7,7 @@ module EV3
       #
       # @param [Integer] Volume of the sound [0..100]
       # @param [Integer] Frequency in Hertz [0..50,000]
-      # @param [Integer] Duration in miliseconds [0..1 Year]
+      # @param [Integer] Duration in milliseconds [0..1 Year]
       def initialize(volume = 50, frequency = 1000, duration = 500)
         super()
 
@@ -15,7 +15,6 @@ module EV3
         validate_range!(frequency, 'frequency', 0..50_000) # 0 - 50,000 Hz
         validate_range!(duration, 'duration', 0..(1000 * 60 * 60 * 24 * 365)) # Up to a year
 
-        self << CommandType::DIRECT_COMMAND_NO_REPLY
         self << ByteCodes::SOUND
         self << SoundSubCodes::TONE
         self << volume.to_ev3_data
