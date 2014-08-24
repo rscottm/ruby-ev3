646ff507c09c19de714aab1683909e25b0500d03
rscottm <rscottmoyer@gmail.com>
2014-08-24 09:10:05 -0700

    Added to manage a piece of a message/response (one among one or more)

---
+++ lib/ev3/command_component.rb
@@ -0,0 +1,83 @@
+module EV3
+  class CommandComponent
+    using EV3::CoreExtensions
+
+    include EV3::Validations::Constant
+    include EV3::Validations::Type
+
+    attr_reader :reply_size
+
+    SIZE_OF = {
+                :byte   => 1,
+                :short  => 2,
+                :int    => 4                    
+              }
+
+    def initialize(object, type, subtype=nil)
+      @object = object
+      @type = type
+      @subtype = subtype
+
+      @parameters = []
+      @reply_types = []
+      @reply_size = 0
+    end
+    
+    def has_reply?
+      @reply_size != 0
+    end
+
+    # Append a byte or multiple bytes to the command
+    #
+    # @param [Integer, Array<Integer>] byte_or_bytes to append to the command
+    def <<(byte_or_bytes)
+      bytes = byte_or_bytes.arrayify
+      bytes.each { |byte| @bytes << byte }
+    end
+
+    def add_parameter(type, value_or_getter)
+      @parameters << [type, value_or_getter]
+      self
+    end
+
+    def add_reply(type, setter)
+      # TODO: Figure out how to handle null terminated strings
+      @reply_types << [type, setter]
+      @reply_size += SIZE_OF[type]
+      self
+    end
+
+    def to_bytes(index=0)
+      @bytes = []
+
+      self << @type
+      self << @subtype if @subtype
+
+      @parameters.each do |type, value_or_getter|
+        self << ArgumentType.const_get(type.to_s.upcase)
+        value = value_or_getter.is_a?(Symbol) ? @object.send(value_or_getter) : value_or_getter
+        self << value.to_little_endian_byte_array(SIZE_OF[type]) 
+      end
+
+      @reply_types.each do |type, setter|
+        self << ArgumentType::GLOBAL_INDEX
+        self << index
+        index += SIZE_OF[type]
+      end
+      @bytes
+    end
+
+    def reply=(bytes)
+      index = 0
+      @reply_types.each do |type, setter|
+        data = case type
+        when :byte then bytes[index]
+        when :short then (bytes[index] | (bytes[index+1] << 8)) 
+        when :int then (bytes[index] | (bytes[index+1] << 8) | (bytes[index+2] << 16) | (bytes[index+3] << 24))           
+        end
+        self << @object.send(setter, data)
+        index += SIZE_OF[type]
+      end      
+    end
+  end
+end
