5b3809506fc1deff687dea25f1d23e8cadad7119
rscottm <rscottmoyer@gmail.com>
2014-08-29 09:52:36 -0700

    Exposed the _pressed? method for polling

--- lib/ev3/actions/button.rb
+++ lib/ev3/actions/button.rb
@@ -1,15 +1,15 @@
 module EV3
   module Actions
     module Button
+      def _pressed?
+        _base(ByteCodes::UI_BUTTON, ButtonSubCodes::PRESSED).add_reply(:boolean, :pressed=)
+      end 
+
       private
-      
+
       def _base(code, subcode=nil)
         CommandComponent.new(self, code, subcode).add_parameter(:byte, button)
       end
-
-      def _pressed?
-        _base(ByteCodes::UI_BUTTON, ButtonSubCodes::PRESSED).add_reply(:byte, :pressed=)
-      end 
     end
   end
 end
