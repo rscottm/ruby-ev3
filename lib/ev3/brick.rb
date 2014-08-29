1c47889625d8d119b8f6c3435cd5d48fac3b665a
rscottm <rscottmoyer@gmail.com>
2014-08-29 09:57:48 -0700

    Polling buttons; command reply

--- lib/ev3/brick.rb
+++ lib/ev3/brick.rb
@@ -35,14 +35,33 @@ module EV3
     def device_list(layer=@layer)
       c = _device_list
       self.execute(c)
-      @device_list = c.replies[0].map{|i| DEVICE_TYPES[i]}
-      @device_list = [[@device_list[0,4],  @device_list[16,4]], 
-                      [@device_list[4,4],  @device_list[20,4]], 
-                      [@device_list[8,4],  @device_list[24,4]], 
-                      [@device_list[12,4], @device_list[28,4]]]
+      unless c.replies[1] == 0
+        @device_list = c.replies[0].map{|i| DEVICE_TYPES[i]}
+        @device_list = [[@device_list[0,4],  @device_list[16,4]], 
+                        [@device_list[4,4],  @device_list[20,4]], 
+                        [@device_list[8,4],  @device_list[24,4]], 
+                        [@device_list[12,4], @device_list[28,4]]]
+      end
       layer == -1 ? @device_list : @device_list[layer]
     end
 
+    def poll_buttons(interval = 0.1, &block)
+      command = Button.constants.map{|b| self.send("button_#{b.downcase}")._pressed?}
+      Button.on_button_changed = block if block_given?
+      @stop_polling = false
+      
+      Thread.new do
+        while(not @stop_polling)
+          self.execute(command)
+          sleep(interval)
+        end
+      end.run
+    end
+    
+    def stop_polling
+      @stop_polling = true
+    end
+          
     # Play a short beep on the EV3
     def beep
       play_tone(50, 1000, 500)
@@ -115,6 +134,7 @@ module EV3
     def execute(command)
       command = Command.new.add_component(command) if command.is_a?(CommandComponent) or command.is_a?(Array)
       self.connection.write(command)
+      command.reply
     end
   end
 end
