e4ea3f898d1af2749bbc65cefe7fd7bd2f39cf1d
rscottm <rscottmoyer@gmail.com>
2015-02-06 22:25:06 -0800

    Now handles System Commands to (different message format)

--- lib/ev3/command.rb
+++ lib/ev3/command.rb
@@ -1,3 +1,47 @@
+#
+# Command Structures
+# 
+# https://github.com/mindboards/ev3sources/blob/78ebaf5b6f8fe31cc17aa5dce0f8e4916a4fc072/lms2012/c_com/source/c_com.h
+#
+#  ,------,------,------,------,------,------,------,------,
+#  |Byte 0|Byte 1|Byte 2|Byte 3|Byte 4|Byte 5|      |Byte n|
+#  '------'------'------'------'------'------'------'------'
+# 
+# System Command Bytes:
+#  Byte 0 – 1: Command size, Little Endian
+#  Byte 2 – 3: Message counter, Little Endian
+#  Byte 4:     Command type
+#  Byte 5:     System Command 
+#  Byte 6 - n: Dependent on System Command
+#
+# System Command Response Bytes:
+#  Byte 0 – 1: Reply size, Little Endian
+#  Byte 2 – 3: Message counter, Little Endian
+#  Byte 4:     Reply type
+#  Byte 5:     System command this is the response to
+#  Byte 6:     Reply status
+#  Byte 7 - n: Response dependent on System Command
+#  
+# Direct Command Bytes:
+#  Byte 0 – 1: Command size, Little Endian
+#  Byte 2 – 3: Message counter, Little Endian
+#  Byte 4:     Command type
+#  Byte 5 - 6: Number of global and local variables (compressed).
+#               Byte 6    Byte 5
+#              76543210  76543210
+#              --------  --------
+#              llllllgg  gggggggg
+#                    gg  gggggggg  Global variables [0..MAX_COMMAND_GLOBALS]
+#              llllll              Local variables  [0..MAX_COMMAND_LOCALS]
+#  Byte 7 - n: Byte codes
+#
+# Direct Command Response Bytes:
+#  Byte 0 – 1: Reply size, Little Endian
+#  Byte 2 – 3: Message counter, Little Endian
+#  Byte 4:     Reply type. see following defines
+#  Byte 5 - n: Response buffer (global variable values)
+#
+
 module EV3
   class Command
     using EV3::CoreExtensions
@@ -16,6 +60,14 @@ module EV3
       @reply_size = 0
       @reply_type = 0
       @error_type = 0
+    end
+    
+    def direct_command?
+      (type & CommandType::SYSTEM_COMMAND == 0)
+    end
+    
+    def system_command?
+      not direct_command?
     end
     
     def has_reply?
@@ -43,7 +95,7 @@ module EV3
       @has_reply = false
       
       @components.each do |c| 
-        @bytes += c.to_bytes(@reply_size)
+        @bytes += c.to_bytes(@reply_size, direct_command? ? :direct : :system)
         if c.has_reply?
           @reply_size += c.reply_size
           @has_reply = true
@@ -60,7 +112,7 @@ module EV3
       
       message = self.sequence_number.to_little_endian_byte_array(2) + 
                   [type] + 
-                  variable_size_bytes + 
+                  (direct_command? ? variable_size_bytes : []) + 
                   @bytes.clone
 
       # The message is proceeded by the message length
@@ -71,42 +123,41 @@ module EV3
       # First two bytes are the sequence_number
       if bytes.size >= 2
         sn = bytes.shift | (bytes.shift << 8) 
-        raise IncorrectSequenceNumber unless sn == sequence_number
+        raise "Incorrect sequence number" unless sn == sequence_number
       else
-        raise NoSequenceNumber
+        raise "No sequence number"
       end
 
       # Third byte is the reply_type
-      raise NoReplyType if bytes.size == 0
+      raise "No reply type" if bytes.size == 0
       reply = bytes.shift
-      raise ErrorReply if reply == @error_type
-      raise IncorrectReplyType unless reply == @reply_type
+      raise "Incorrect reply type" unless [@reply_type, @error_type].include?(reply)
 
-      # The remaining bytes are the reply
-      raise IncorrectReplySize if bytes.size != @reply_size
-      
       @replies = []
 
-      @components.each do |c|
-        if c.has_reply?
-           c.reply = bytes[0..(c.reply_size-1)]
-           bytes = bytes[c.reply_size..-1]
-           @replies += c.replies
+      if direct_command?
+        raise("Command returned an error") if reply == @error_type
+        # The remaining bytes are the reply
+        raise "Incorrect reply size" if bytes.size != @reply_size
+        
+        @components.each do |c|
+          if c.has_reply?
+             c.reply = bytes[0..(c.reply_size-1)]
+             bytes = bytes[c.reply_size..-1]
+             @replies += c.replies
+          end
         end
+      else
+        raise("Command returned error: #{bytes[1]}") if reply == @error_type
+        cmd = bytes.shift
+        raise("Commmand return does not match!") unless cmd == @components[0].type
+        @components[0].reply = bytes
+        @replies += @components[0].replies
       end
     end
     
     def reply
       replies[0] if @replies
-    end
-
-    # Raises an exception if the value isn't found in the range
-    #
-    # @param [Integer] value to check against the range
-    # @param [String] variable_name for the exception message
-    # @param [Range<Integer>] range the value should be in
-    def validate_range!(value, variable_name, range)
-      raise(ArgumentError, "#{variable_name} should be between #{range.min} and #{range.max}") unless range.include?(value)
     end
 
     private
